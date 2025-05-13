154b3f451b60b0ac448a001a822c9c3f8bf80157
Raffaele <raffaelenini99@gmail.com>
2025-05-13 21:19:03 +0200

    Added best time and icons

--- App.jsx
+++ App.jsx
@@ -7,6 +7,10 @@ export default function App() {
     const [dice, setDice] = useState(() => generateAllNewDice())
     const [rolls, setRolls] = useState(0)
     const [timer, setTimer] = useState(0)
+    const [currentScore, setCurrentScore] = useState(() => {
+        const bestScoreStr = localStorage.getItem("bestScore")
+        return bestScoreStr ? JSON.parse(bestScoreStr) : null
+    })
 
     const buttonRef = useRef(null)
     const intervalRef = useRef(null)
@@ -18,11 +22,12 @@ export default function App() {
         if (gameWon) {
             buttonRef.current.focus()
             clearInterval(intervalRef.current)
+            updateBestScore()
         }
     }, [gameWon])
 
     useEffect(() => {
-        if (rolls != 1) {
+        if (rolls !== 1) {
             return
         }
         
@@ -64,6 +69,20 @@ export default function App() {
                 die
         ))
     }
+    
+    function updateBestScore() {
+        const thisGameScore = { rolls, time: timer }
+        const bestScoreStr = localStorage.getItem("bestScore")
+        const bestScore = bestScoreStr ? JSON.parse(bestScoreStr) : null
+
+        if (
+            !bestScore ||
+            thisGameScore.time < bestScore.time
+        ) {
+            localStorage.setItem("bestScore", JSON.stringify(thisGameScore))
+            setCurrentScore(thisGameScore)
+        }
+    }
 
     const diceElements = dice.map(dieObj => (
         <Die
@@ -81,17 +100,18 @@ export default function App() {
                 {gameWon && <p>Congratulations! You won! Press "New Game" to start again.</p>}
             </div>
             <h1 className="title">Tenzies</h1>
+            <div className="score">
+                {rolls > 0 ? <p><i className="fa-solid fa-dice"></i> Rolls: {rolls}</p> : null}
+                {rolls > 0 ? <p><i className="fa-regular fa-clock"></i> Time: {timer > 60 ? `01:${timer - 60}` : `00:${timer}`}</p> : null}
+            </div>
             <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className="dice-container">
                 {diceElements}
             </div>
-            <div className="score">
-                {rolls > 0 ? <p>Rolls: {rolls}</p> : null}
-                {rolls > 0 ? <p>Time: {timer > 60 ? `01:${timer - 60}` : `00:${timer}`}</p> : null}
-            </div>
             <button ref={buttonRef} className="roll-dice" onClick={rollDice}>
                 {gameWon ? "New Game" : "Roll"}
             </button>
+            {currentScore ? <p>🏆 Best Score: Time: {currentScore.time > 60 ? `01:${currentScore.time - 60}` : `00:${currentScore.time}`} Rolls: {currentScore.rolls}</p> : null}
         </main>
     )
 }
